4a41a9401d8713dd31c79efe8eed507bad81a601
Tenzing Rabgyal <rabgyaltenzing@gmail.com>
2019-08-14 16:32:15 -0400

    Seventh commit

---
+++ 160-LetsCodeCountAndSum/160-LetsCodeCountAndSum/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  160-LetsCodeCountAndSum
+//
+//  Created by Tenzing Rabgyal on 8/14/19.
+//  Copyright © 2019 Tenzing Rabgyal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
